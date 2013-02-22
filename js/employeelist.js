22c5e9524e0c3dd05cb22b23817b532f70e0c3d9
Steef Janssen <janssenman@gmail.com>
2013-02-22 14:58:14 +0100

    First time commit

---
+++ js/employeelist.js
@@ -0,0 +1,23 @@
+var serviceURL = "http://localhost/examples/jqm/employee_app/services/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+
+function getEmployeeList() {
+	$.getJSON(serviceURL + 'getemployees.php', function(data) {
+		$('#employeeList li').remove();
+		console.log(data);
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+					'<img src="pics/' + employee.picture + '"/>' +
+					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+					'<p>' + employee.title + '</p>' +
+					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+		});
+		$('#employeeList').listview('refresh');
+	});
+}
---
+++ services/config.php
@@ -0,0 +1,8 @@
+<?php
+	
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = 'ee21js';
+	$dbname = 'employee';
+
+?>
